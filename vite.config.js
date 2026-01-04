fc771234509a59e4712df14ba325e60721bcf02b
Binh <phanngocbinh515@gmail.com>
2026-01-04 19:44:17 +0700

    fix lai CD

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base: '/react-vite-hoc/',
+  base: "/react-vite-hoc/",
   plugins: [react()],
 });
